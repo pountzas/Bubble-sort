30e73faa9c04f7811ddcf7cf4572faad7494f5cf
nikos@pountzas.gr <72627495+pountzas@users.noreply.github.com>
2021-03-17 19:43:45 +0200

    finish 1st assignment

---
+++ bubble_sort.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+def bubble_sort(arr)
+  arr_sort = arr.sort
+  while arr != arr_sort
+    (0..arr.length - 2).each do |i|
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
+    end
+  end
+end
+
+puts bubble_sort([4, 3, 78, 2, 0, 2])
