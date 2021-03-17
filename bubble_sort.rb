189060e0c28470912d9eac414025910a6b50068f
francis uloko <francisuloko@yahoo.com>
2021-03-17 20:38:08 +0100

    Add bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,6 +10,19 @@ def bubble_sort(arr)
   print arr
 end
 
-bubble_sort([4, 3, 78, 2, 0, 2])
+# bubble_sort([4, 3, 78, 2, 0, 2])
 
 # bubble_sort_by
+def bubble_sort_by(arr)
+  arr_sort = arr.sort
+  while arr != arr_sort
+    (0..arr.length - 2).each do |i|
+      comparison = yield(arr[i], arr[i + 1])
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if comparison.positive?
+    end
+  end
+  p arr
+end
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
