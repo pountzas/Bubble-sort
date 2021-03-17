16a392e17fd200a1b6112ed89b91aa97319cd0e9
nikos@pountzas.gr <72627495+pountzas@users.noreply.github.com>
2021-03-17 20:00:45 +0200

    fix print on def bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,9 +7,9 @@ def bubble_sort(arr)
       arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
     end
   end
+  print arr
 end
 
-puts bubble_sort([4, 3, 78, 2, 0, 2])
+bubble_sort([4, 3, 78, 2, 0, 2])
 
-#bubble_sort_by
-
+# bubble_sort_by
